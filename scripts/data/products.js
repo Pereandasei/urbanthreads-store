95e150b9c9dda48c3d7d9f4f0ffd397264ce8fdc
Pereandasei <pereandasei@gmail.com>
2025-08-08 15:26:18 +0100

    Update products.js

--- scripts/data/products.js
+++ scripts/data/products.js
@@ -2,35 +2,36 @@ export const products = [{
     id: '82fef1f1-6eaf-41ae-893d-a52575736697',
     name:'UrbanThreads classic tee',
     priceCents: 2500,
-    image: '../../images/tees.jpg'
+    image: './images/tees.jpg'
 },
 {   
     id: '662a80c0-e955-4852-a3c6-c61c62249e56',
     name:'UrbanThreads joggers',
     priceCents: 6000,
-    image: '../../images/joggers.jpg'
+    image: './images/joggers.jpg'
 },
 {   
     id: '817171f2-8a56-4c64-8f29-e9495fde0eda',
     name:'UrbanThreads hoodie',
     priceCents: 5000,
-    image: '../../images/hoodie.jpg'
+    image: './images/hoodie.jpg'
 },
 {   
     id: 'e3c0efb0-7ade-4f9e-b443-1e11db2150a7',
     name:'UrbanThreads snapback',
     priceCents: 3000,
-    image: '../../images/cap.jpg'
+    image: './images/cap.jpg'
 },
 {   id: '1df3bfc5-71e6-41f1-8d0d-eb1f36467800',
     name:'UrbanThreads backpack',
     priceCents: 4500,
-    image: '../../images/backpack.jpg'
+    image: './images/backpack.jpg'
 },
 {   
     id: 'cb08efaf-8742-41cb-aca3-523893752e29',
     name:'UrbanThreads socks',
     priceCents: 1500,
-    image: '../../images/socks.jpg'
+    image: './images/socks.jpg'
 }
 ];
+
